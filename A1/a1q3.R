4966c8ca0851a632165a25560127c022b2c9bc80
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-02-14 17:53:40 +0000

    anno

--- A1/a1q3.R
+++ A1/a1q3.R
@@ -15,6 +15,8 @@ devAskNewPage(ask = FALSE)
 par(ask = F)
 #options(mc.cores = 1)
 
+par(mfrow = c(1,1))
+
 abalone <- fread('data/abalone.csv')
 names(abalone) <- make.names(names(abalone))
 abalone$Age <- abalone$Rings + 1.5
@@ -74,7 +76,8 @@ b_lm_data <-
     X = X,
     y = abalone$Age,
     N_new = 1,
-    x_new = as.array(mm_new)
+    x_new = as.array(mm_new),
+    p_params = c(1e4, 1e-1, 1e-1, 1/27)
   )
 
 b_lm <- stan_model(file = 'a1q3.stan')
@@ -84,26 +87,26 @@ b_lm_fit <- sampling(
   data = b_lm_data,
   chains = 7,
   control = list(adapt_delta = 0.8),
-  iter = 8000
+  iter = 16000
 )
 plot(b_lm_fit)
 b_lm_fit
 
 smps <- extract(b_lm_fit)
-tp <- traceplot(b_lm_fit, pars = c("beta", "sigma"))
-tp
+# tp <- traceplot(b_lm_fit, pars = c("beta", "sigma"))
+# tp
 
 b_lm_coda <- As.mcmc.list(b_lm_fit)
 gelman.plot(b_lm_coda, ask=FALSE)
-gelman.diag(b_lm_coda)
-plot(b_lm_coda, ask = FALSE)
+# gelman.diag(b_lm_coda)
+# plot(b_lm_coda, ask = FALSE)
 
 eap_beta <- matrix(colMeans(smps$beta), ncol = 1)
 
 b_lm_age <- X %*% eap_beta
 
 n_dens <-
-  ggplot(data = data.frame(samp = smps$y_new), aes(x = samp)) + geom_density()
+  ggplot(data = data.frame(samp = smps$y_new), aes(x = samp)) + geom_density() + coord_cartesian(xlim = c(-10,10))
 n_dens
 
 pred_eap <- mean(smps$y_new)
@@ -121,6 +124,105 @@ lp_lm <- log_posterior(b_lm_fit)
 mcmc_nuts_divergence(np_lm, lp_lm)
 mcmc_nuts_acceptance(np_lm, lp_lm)
 
-pairs(b_lm_fit, pars = c("beta[1]", "beta[2]", "beta[3]"))
-pairs(b_lm_fit, pars = c("beta[4]", "beta[5]", "beta[6]"))
-pairs(b_lm_fit, pars = c("beta[7]", "beta[8]", "beta[9]", "beta[10]"))
+# pairs(b_lm_fit, pars = c("beta[1]", "beta[2]", "beta[3]"))
+# pairs(b_lm_fit, pars = c("beta[4]", "beta[5]", "beta[6]"))
+# pairs(b_lm_fit, pars = c("beta[7]", "beta[8]", "beta[9]", "beta[10]"))
+
+color_scheme_set("viridisD")
+mcmc_trace(posterior_lm, pars = c("beta[1]", "beta[2]", "beta[3]", "beta[4]", "beta[5]", "beta[6]", "beta[7]", "beta[8]", "beta[9]", "beta[10]", "sigma"), np = np_lm, size = 0.05)
+
+# b_lm_data_test <-
+#   list(
+#     N = nrow(X),
+#     K = ncol(X),
+#     X = X,
+#     y = abalone$Age,
+#     N_new = 1,
+#     x_new = as.array(mm_new),
+#     p_params = c(1e1, 7, 30)
+#   )
+# 
+# b_lm_fit_prior_sensitivity <- sampling(
+#   b_lm,
+#   data = b_lm_data_test,
+#   chains = 7,
+#   control = list(adapt_delta = 0.8),
+#   iter = 3000
+# )
+# 
+# b_lm_fit_prior_sensitivity
+# 
+# posterior_lm_psa <- as.array(b_lm_fit_prior_sensitivity)
+# np_lm_psa <- nuts_params(b_lm_fit_prior_sensitivity)
+# lp_lm_psa <- log_posterior(b_lm_fit_prior_sensitivity)
+# 
+# mcmc_nuts_divergence(np_lm_psa, lp_lm_psa)
+# mcmc_nuts_acceptance(np_lm_psa, lp_lm_psa)
+# 
+# pairs(b_lm_fit_prior_sensitivity, pars = c("beta[1]", "beta[2]", "beta[3]"))
+# pairs(b_lm_fit_prior_sensitivity, pars = c("beta[4]", "beta[5]", "beta[6]"))
+# pairs(b_lm_fit_prior_sensitivity, pars = c("beta[7]", "beta[8]", "beta[9]", "beta[10]"))
+# 
+# color_scheme_set("viridisD")
+# mcmc_trace(posterior_lm_psa, pars = c("beta[1]", "beta[2]", "beta[3]", "beta[4]", "beta[5]", "beta[6]", "beta[7]", "beta[8]", "beta[9]", "beta[10]", "sigma"), np = np_lm_psa, size = 0.05)
+
+
+quantile_set <- seq(0.01, 0.99, by = 0.01)
+data_q <- quantile(abalone$Age, quantile_set)
+bayes_q <- quantile(b_lm_age, quantile_set)
+lm_q <- quantile(fitted(f_lm), quantile_set)
+
+quantile_df <- data.frame(obs = data_q, b = bayes_q, l = lm_q)
+ggqq <- ggplot(data = quantile_df, aes(y = obs)) + coord_cartesian(xlim = c(-10, 10), ylim = c(-10, 10)) +
+  geom_point(aes(x = b, colour = "Bayesian Robust Linear Model")) + 
+  geom_point(aes(x = l, colour = "Normal Linear Model")) + 
+  theme_minimal() +
+  labs(x = "Fitted Quantiles", y = "Sample Quantiles") + geom_abline(slope = 1, intercept = 0) + scale_colour_discrete(name = "Distribution")
+ggqq
+
+#####
+
+# b_lm_normal <- stan_model(file = 'a1q3normal.stan')
+# 
+# b_lm_fit_normal <- sampling(
+#   b_lm_normal,
+#   data = b_lm_data,
+#   chains = 7,
+#   control = list(adapt_delta = 0.8),
+#   iter = 4000
+# )
+# plot(b_lm_fit_normal)
+# b_lm_fit_normal
+# 
+# smps_norm <- extract(b_lm_fit_normal)
+# 
+# eap_beta_norm <- matrix(colMeans(smps_norm$beta), ncol = 1)
+# 
+# b_lm_age_norm <- X %*% eap_beta_norm
+# 
+# n_dens_norm <-
+#   ggplot(data = data.frame(samp = smps_norm$y_new), aes(x = samp)) + geom_density() + coord_cartesian(xlim = c(-10,10))
+# n_dens_norm
+# 
+# quantile_set <- seq(0.01, 0.99, by = 0.01)
+# data_q <- quantile(abalone$Age, quantile_set)
+# bayes_q <- quantile(b_lm_age, quantile_set)
+# lm_q <- quantile(fitted(f_lm), quantile_set)
+# bayes_n_q <- quantile(b_lm_age_norm, quantile_set)
+# 
+# quantile_df <- data.frame(obs = data_q, b = bayes_q, l = lm_q, bn = bayes_n_q)
+# ggqq <- ggplot(data = quantile_df, aes(y = obs)) + coord_cartesian(xlim = c(-10, 10), ylim = c(-10, 10)) +
+#   geom_point(aes(x = b, colour = "Bayesian Robust Linear Model")) + 
+#   geom_point(aes(x = l, colour = "Normal Linear Model")) + 
+#   geom_point(aes(x = bn, colour = "Bayesian Normal Linear Model")) + 
+#   theme_minimal() +
+#   labs(x = "Fitted Quantiles", y = "Sample Quantiles") + geom_abline(slope = 1, intercept = 0) + scale_colour_discrete(name = "Distribution")
+# ggqq
+
+n_dens <-
+  ggplot(data = data.frame(samp = smps$y_new), aes(x = samp)) +
+  geom_density(size = 1, color = "darkblue", fill = "lightblue", alpha = 0.3) +
+  coord_cartesian(xlim = c(-10,10)) +
+  theme_minimal() +
+  labs(title = "Posterior Predictive for New Data", x = "Mean-Corrected Age", y = "Density")
+n_dens
--- A1/a1q3normal.stan
+++ A1/a1q3normal.stan
@@ -5,21 +5,19 @@ data {
   vector[N] y;
   int<lower=0> N_new;
   matrix[N_new, K] x_new;
-  vector[4] p_params;
+  vector[3] p_params;
 }
 parameters {
   vector[K] beta;
   real<lower=0> sigma;
-  real<lower=0> nu;
 }
 model {
-  nu ~ exponential(p_params[4]);
   beta ~ normal(0, p_params[1]);
   sigma ~ inv_gamma(p_params[2], p_params[3]);
-  y ~ student_t(nu+1, X * beta, sigma);
+  y ~ normal(X * beta, sigma);
 }
 generated quantities {
   vector[N_new] y_new;
   for (n in 1:N_new)
-    y_new[n] = student_t_rng(nu+1, x_new[n] * beta, sigma);
+    y_new[n] = normal_rng(x_new[n] * beta, sigma);
 }
