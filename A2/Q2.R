f1208ac01ed90cd023393217279e3a127dfcb74b
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-03-24 23:31:09 +0000

    sq2

--- A2/Q2.R
+++ A2/Q2.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(ggplot2)
+library(dplyr)
 
 library(rstan)
 rstan_options(auto_write = TRUE)
@@ -14,9 +15,131 @@ library(bayesplot)
 #loading and eda
 avalanches_prop <- fread(file = "data/Avalanches_part2.csv")
 avalanches_prop[, Event_ID := NULL]
-avalanches_prop[, Snow_meters := round(Snow_total / 100)]
-avalanches_prop[, Snow_fnights := round(Snow_days / 14)]
-
+avalanches_prop[, Snow_meters := Snow_total / 100]
+avalanches_prop[, Snow_fnights := Snow_days / 14]
+avalanches_prop[, death_prop := Deaths / Hit]
+avalanches_prop[, Geo_space := as.factor(Geo_space)]
+avalanches_prop[, Rec.station := as.factor(Rec.station)]
 cor(avalanches_prop[, .(Season, Snow_meters, Snow_fnights)])
 #####
-stan_binomial_glm <- stan_model(file = "stan/binomial_glm.stan")
+stan_binomial_glm_reff <-
+  stan_model(file = "stan/binomial_glm_randomeffects.stan")
+
+submin <- function(x){
+  m <- min(x)
+  x <- x - m
+  attributes(x) <- list("scaled:submin" = m)
+  return(x)
+}
+
+cont_vars <- c("Snow_meters", "Snow_fnights")#variables to centre
+avalanches_prop[,(cont_vars) := lapply(.SD, scale, scale = FALSE), .SDcols = cont_vars]#centre variables
+tm_vars <- c("Season")
+avalanches_prop[,(tm_vars) := lapply(.SD, submin), .SDcols = tm_vars]
+
+
+X_fixedeff <-
+  model.matrix(death_prop ~ Season + Snow_meters + Snow_fnights - 1, data = avalanches_prop)
+X_randomeff <-
+  model.matrix(death_prop ~ Geo_space - 1, data = avalanches_prop)
+success <- avalanches_prop[, Deaths]
+trials <- avalanches_prop[, Hit]
+
+
+stan_binomial_glm_reff_data <-
+  list(
+    success = success,
+    trials = trials,
+    X_f = X_fixedeff,
+    X_r = X_randomeff,
+    N = length(success),
+    P_f = ncol(X_fixedeff),
+    P_r = ncol(X_randomeff),
+    n_params = c(0, sqrt(10))
+  )
+
+stan_binomial_glm_reff_s <-
+  sampling(
+    stan_binomial_glm_reff,
+    data = stan_binomial_glm_reff_data,
+    chains = 4,
+    control = list(adapt_delta = 0.9),
+    iter = 10000#,
+    #init_r = 0.1
+  )
+reff_coda <- As.mcmc.list(stan_binomial_glm_reff_s, pars = c("beta_r", "beta_f"))
+gelman.plot(reff_coda, ask = FALSE)
+
+plot_diag_objects <- function(stanfit){
+  list(post = as.array(stanfit),
+       lp = log_posterior(stanfit),
+       np = nuts_params(stanfit))
+}
+
+plot_diag <- function(stanfit, pars){
+  ps <- vars(starts_with(pars))
+  post <- as.array(stanfit)
+  lp <- log_posterior(stanfit)
+  np <- nuts_params(stanfit)
+  p1 <- mcmc_parcoord(post, np = np, pars = ps)
+  p2 <- mcmc_pairs(post, np = np, pars = ps)
+  p3 <- mcmc_trace(post, pars = ps, np = np)
+  p4 <- mcmc_nuts_divergence(np, lp)
+  p5 <- mcmc_nuts_energy(np)
+  list(p1, p2, p3, p4, p5)
+}
+
+#mcmc_trace(stan_binomial_glm_reff_s, pars = vars(starts_with("beta")))
+
+#####
+#sans snow fortnights
+
+X_f_nsf <- model.matrix(death_prop ~ Season + Snow_meters - 1, data = avalanches_prop)
+
+stan_binomial_glm_reff_nsf_data <-
+  list(
+    success = success,
+    trials = trials,
+    X_f = X_f_nsf,
+    X_r = X_randomeff,
+    N = length(success),
+    P_f = ncol(X_f_nsf),
+    P_r = ncol(X_randomeff),
+    n_params = c(0, sqrt(10))
+  )
+
+stan_binomial_glm_reff_nsf_s <-
+  sampling(
+    stan_binomial_glm_reff,
+    data = stan_binomial_glm_reff_nsf_data,
+    chains = 4,
+    control = list(adapt_delta = 0.9),
+    iter = 10000#,
+    #init_r = 0.1
+  )
+
+#####
+#hieratchical on station, sans snow fortnights
+X_r_station <- model.matrix(death_prop ~ Rec.station - 1, data = avalanches_prop)
+
+stan_binomial_glm_reff_station_data <-
+  list(
+    success = success,
+    trials = trials,
+    X_f = X_f_nsf,
+    X_r = X_r_station,
+    N = length(success),
+    P_f = ncol(X_f_nsf),
+    P_r = ncol(X_r_station),
+    n_params = c(0, sqrt(10))
+  )
+
+stan_binomial_glm_reff_station_s <-
+  sampling(
+    stan_binomial_glm_reff,
+    data = stan_binomial_glm_reff_station_data,
+    chains = 4,
+    control = list(adapt_delta = 0.9),
+    iter = 10000#,
+    #init_r = 0.1
+  )
---
+++ A2/stan/binomial_glm_randomeffects.stan
@@ -0,0 +1,36 @@
+data {
+  int<lower=0> N;
+  int<lower=0> P_f;
+  int<lower=0> P_r;
+  
+  int<lower=0> success[N];
+  int<lower=1> trials[N];
+  
+  matrix[N, P_f] X_f;
+  matrix[N, P_r] X_r;
+  
+  vector[2] n_params;
+}
+
+parameters {
+  vector[P_f] beta_f;
+  vector[P_r] sn_vec;
+  real<lower=0,upper=10> reff_sdv;
+}
+
+transformed parameters{
+  vector[P_r] beta_r = reff_sdv * sn_vec;
+  vector[N] lg_p = X_f * beta_f + X_r * beta_r;
+}
+
+model {
+  reff_sdv ~ uniform(0, 10);
+  sn_vec ~ std_normal();
+  beta_f ~ normal(n_params[1], n_params[2]);
+  success ~ binomial(trials, inv_logit(lg_p));
+}
+generated quantities{
+  int data_ppred[N] = binomial_rng(trials, inv_logit(lg_p));
+  vector[N] data_prop = inv_logit(lg_p);
+}
+
