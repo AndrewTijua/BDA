57ed111a24a547fcdd65c2a7b7130426ea58e1f2
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-03-25 16:42:51 +0000

    added jags

--- A2/Q1.R
+++ A2/Q1.R
@@ -26,30 +26,60 @@ avalanches[, EWS := as.factor(EWS)]
 
 base_plot <-
   ggplot(data = as.data.frame(avalanches), aes(colour = EWS)) + theme_minimal()
-base_plot + geom_line(aes(x = Season, y = Rep.events))
-base_plot + geom_line(aes(x = Season, y = Deaths))
+base_plot + geom_line(aes(x = Season, y = Rep.events, group = F))
+base_plot + geom_line(aes(x = Season, y = Deaths, group = F))
 base_plot + geom_boxplot(aes(x = EWS, y = Deaths), colour = "black")
+
+
+cor_boot <- function(data, index) {
+  dt_s <- data[index, ]
+  return(cor(dt_s))
+}
 
 cor(avalanches[(EADS1 == FALSE &
                   EADS2 == FALSE), .(Rep.events, Deaths)])
 cor(avalanches[EADS1 == TRUE, .(Rep.events, Deaths)])
 cor(avalanches[EADS2 == TRUE, .(Rep.events, Deaths)])
+
+bs1 <- boot::boot(avalanches[(EADS1 == FALSE &
+                                EADS2 == FALSE),
+                             .(Rep.events, Deaths)]
+                  , cor_boot, R = 1e3)
+bs2 <- boot::boot(avalanches[(EADS1 == TRUE),
+                             .(Rep.events, Deaths)]
+                  , cor_boot, R = 1e3)
+bs3 <- boot::boot(avalanches[(EADS2 == TRUE),
+                             .(Rep.events, Deaths)]
+                  , cor_boot, R = 1e3)
+boot::boot.ci(bs1,
+              index = 2,
+              type = "perc",
+              conf = 0.9)
+boot::boot.ci(bs2,
+              index = 2,
+              type = "perc",
+              conf = 0.9)
+boot::boot.ci(bs3,
+              index = 2,
+              type = "perc",
+              conf = 0.9)
 #####
 #b
 to_model <- avalanches[, .(Deaths, Rep.events, EADS1, EADS2)]
-model_mat <- model.matrix(Deaths ~ ., data = to_model)#no intercept as cannot have deaths without avalanche
+model_mat <-
+  model.matrix(Deaths ~ ., data = to_model)#no intercept as cannot have deaths without avalanche
 
-model_mat <- model_mat[,-1]
+model_mat <- model_mat[,]
 out_names = colnames(model_mat)
 #no need to centre as discrete
 
 #new data
-# X_new = matrix(c(1, 20, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1),
-#                nrow = 4,
-#                byrow = T)
-X_new = matrix(c(20, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1),
+X_new = matrix(c(1, 20, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1),
                nrow = 4,
                byrow = T)
+# X_new = matrix(c(20, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1),
+#                nrow = 4,
+#                byrow = T)
 N_new = nrow(X_new)
 #check, should be similar
 f_glm <-
@@ -81,7 +111,8 @@ stan_poisson_glm_s <-
 
 post_params <- extract(stan_poisson_glm_s, "lambda")[[1]]
 colnames(post_params) <- out_names
-apply(post_params, 2, summary)
+exp_post_params <- exp(post_params)
+apply(exp_post_params, 2, summary)
 
 p_pred <- extract(stan_poisson_glm_s, "y_new")[[1]]
 mean(p_pred[, 1] < 15)
@@ -96,10 +127,14 @@ plikrar <- function(x, data) {
   sum(dpois(data, x, log = T))
 }
 sampling_rates <- extract(stan_poisson_glm_s, "rate")[[1]]
-sr_like <- apply(sampling_rates, 1, plikrar, avalanches$Deaths)#calculate log likelihoods of each sampling
-sr_like_mean <- mean(sr_like)#calculate mean log likelihood of samples
-eap <- colMeans(sampling_rates)#calculate posterior means of rates (not parameters)
-p_mean_like <- sum(dpois(avalanches$Deaths, eap, log = T))#calculate log likelihood of EAP
+sr_like <-
+  apply(sampling_rates, 1, plikrar, avalanches$Deaths)#calculate log likelihoods of each sampling
+sr_like_mean <-
+  mean(sr_like)#calculate mean log likelihood of samples
+eap <-
+  colMeans(sampling_rates)#calculate posterior means of rates (not parameters)
+p_mean_like <-
+  sum(dpois(avalanches$Deaths, eap, log = T))#calculate log likelihood of EAP
 dbar <- -2 * sr_like_mean#expected deviance
 pd <- dbar + 2 * p_mean_like#calculate penalty
 dic <- pd + dbar#give dic
@@ -111,21 +146,29 @@ dic <- pd + dbar#give dic
 # xm <- dp_av - m_deaths
 # lnfactor <- 2/(xm)^2
 # inffactor <- dp_av / m_deaths
-# beta_p <- 
+# beta_p <-
 # mfc <- exp(xm * inffactor)
 # mfc_p <- plnorm(mfc, 0, 2)
 avno <- avalanches$Rep.events
 avde <- avalanches$Deaths
 mede <- mean(avde)
-psi <- avde/mede
-beta <- log(psi)/(avno - mean(avno))
+psi <- avde / mede
+beta <- log(psi) / (avno - mean(avno))
 psi_p <- dlnorm(psi, 0, 2)
-beta_p <- dnorm(beta, 0, (avno-mean(avno))^(-2))
+beta_p <- dnorm(beta, 0, (avno - mean(avno)) ^ (-2))
 #####
-stan_poisson_glm_exvar <- stan_model(file = "stan/poisson_glm_exvar.stan")
+stan_poisson_glm_exvar <-
+  stan_model(file = "stan/poisson_glm_exvar.stan")
+
+model_mat <- model_mat[,-1]#messes with exvar
+out_names = colnames(model_mat)
+
+X_new = matrix(c(20, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1),
+               nrow = 4,
+               byrow = T)
 
 ym <- data.frame(ym = as.factor(avalanches$Season))
-yim <- model.matrix(~ . -1, ym)
+yim <- model.matrix( ~ . - 1, ym)
 
 stan_poisson_glm_exvar_data <-
   list(
@@ -151,7 +194,8 @@ stan_poisson_glm_exvar_s <-
     init_r = 1
   )
 
-post_params_exvar <- extract(stan_poisson_glm_exvar_s, "lambda")[[1]]
+post_params_exvar <-
+  extract(stan_poisson_glm_exvar_s, "lambda")[[1]]
 colnames(post_params_exvar) <- out_names
 apply(post_params_exvar, 2, summary)
 
@@ -162,10 +206,14 @@ plikrar <- function(x, data) {
   sum(dpois(data, x, log = T))
 }
 sampling_rates_exv <- extract(stan_poisson_glm_exvar_s, "rate")[[1]]
-sr_like_exv <- apply(sampling_rates_exv, 1, plikrar, avalanches$Deaths)#calculate log likelihoods of each sampling
-sr_like_mean_exv <- mean(sr_like_exv)#calculate mean log likelihood of samples
-eap_exv <- colMeans(sampling_rates_exv)#calculate posterior means of rates (not parameters)
-p_mean_like_exv <- sum(dpois(avalanches$Deaths, eap_exv, log = T))#calculate log likelihood of EAP
+sr_like_exv <-
+  apply(sampling_rates_exv, 1, plikrar, avalanches$Deaths)#calculate log likelihoods of each sampling
+sr_like_mean_exv <-
+  mean(sr_like_exv)#calculate mean log likelihood of samples
+eap_exv <-
+  colMeans(sampling_rates_exv)#calculate posterior means of rates (not parameters)
+p_mean_like_exv <-
+  sum(dpois(avalanches$Deaths, eap_exv, log = T))#calculate log likelihood of EAP
 dbar_exv <- -2 * sr_like_mean_exv#expected deviance
 pd_exv <- dbar_exv + 2 * p_mean_like_exv#calculate penalty
 dic_exv <- pd_exv + dbar_exv#give dic
---
+++ A2/jags/Q1jags.R
@@ -0,0 +1,67 @@
+library(data.table)
+library(ggplot2)
+
+library(rjags)
+library(coda)
+library(bayesplot)
+
+
+#####
+#a
+avalanches <- fread(file = "data/Avalanches.csv")
+avalanches <- avalanches[Rep.events > 0]
+avalanches[, ':=' (EADS1 = (Season >= 1994 &
+                              Season <= 2003),
+                   EADS2 = (Season >= 2004))]
+
+avalanches[Season %in% c(1986, 1994, 2004)]
+
+avalanches[, EWS := 1 + EADS1 + 2 * EADS2]
+avalanches[, EWS := as.factor(EWS)]
+
+pglm_data <-
+  list(
+    n = nrow(avalanches),
+    rep = avalanches$Rep.events,
+    w1 = avalanches$EADS1,
+    w2 = avalanches$EADS2,
+    death = avalanches$Deaths
+  )
+
+res.a <-
+  jags.model(
+    file = "jags/poisson.jags",
+    data = pglm_data,
+    n.chains = 4,
+    quiet = T
+  )
+update(res.a, n.iter = 1e4)
+res.b <-
+  coda.samples(
+    res.a,
+    variable.names = c("intercept", "beta_rep", "beta_w1", "beta_w2"),
+    n.iter = 1e4
+  )
+summary(res.b)
+dic.samples(model = res.a,
+            n.iter = 1e4,
+            type = 'pD')
+
+res.a.ev <-
+  jags.model(
+    file = "jags/poisson_exvar.jags",
+    data = pglm_data,
+    n.chains = 4,
+    quiet = T
+  )
+update(res.a, n.iter = 1e4)
+res.b.ev <-
+  coda.samples(
+    res.a.ev,
+    variable.names = c("beta_rep", "beta_w1", "beta_w2"),
+    n.iter = 1e4
+  )
+summary(res.b.ev)
+dic.samples(model = res.a.ev,
+            n.iter = 1e4,
+            type = 'pD')
